5d296f0b9e4128426c1b0c73401db83a21b3016b
anandkumar-coSet status <anandpandey62099@gmail.com>
2024-11-04 00:17:50 +0530

    theme chaging

---
+++ themechange/src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { ThemeContextProvider } from "./context/theme";
+import ThemeBtn from "./component/ThemeBtn";
+import Card from "./component/Card";
+
+function App() {
+  // const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light");
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("dark", "light");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+  return (
+    <ThemeContextProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeContextProvider>
+  );
+}
+
+export default App;
