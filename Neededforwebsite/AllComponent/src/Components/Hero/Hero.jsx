6744645cf6e9df361bc7df05f53f34718b19e852
anandkumar-co <anandkumar2003feb@gmail.com>
2025-05-23 10:22:52 +0530

    pokemon box created now new serch pokemon boxes created for filtering

--- Neededforwebsite/AllComponent/src/Components/Hero/Hero.jsx
+++ Neededforwebsite/AllComponent/src/Components/Hero/Hero.jsx
@@ -1,26 +1,70 @@
 import { useEffect, useState } from "react";
-import "./hero.css";
+import "./Hero.css";
 import Item from "../Items/Item";
 function Hero() {
   const[apiData, setApiData]=useState(null);
-  useEffect(()=>{
-    fetch("https://pokeapi.co/api/v2/pokemon/1/")
+  const[loadingState,setLoadindState]=useState(true);
+  const[errorstate,setErrorstate]=useState("");
+  const Api="https://pokeapi.co/api/v2/pokemon?limit=100";
+function fetchApi(Api){
+ fetch(Api)
     .then((res)=>res.json())
     .then((data)=>{
       setApiData(data);
-      console.log(data);
+      setLoadindState(false);
     })
-    .catch((err)=>{console.log(err)})
+    .catch((err)=>{setErrorstate("Error fetching data");})
+  }
+
+  useEffect(()=>{
+   fetchApi(Api)
   },[])
-  console.log({apiData});
-  if(apiData){
-  return (
-    
+  
+  if(loadingState){
+    return(
+      <div className="hero">
+        <h1>POKEMON</h1>
+        <div className="pokemon-box">
+          <ul>
+            <li>
+              <Item/>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+  if(errorstate){
+    return(
+      <div className="hero">
+        <h1>POKEMON</h1>
+        <div className="pokemon-box">
+          <ul>
+            <li>
+              <Item/>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+  return (  
     <div className="hero">
-      <Item apiData={apiData}/>
+      <h1>POKEMON</h1>
+      <div className="pokemon-box">
+        <ul>
+          {apiData.results.map((item,i)=>{
+            return(
+              <li key={i}>
+                <Item name={item.name} url={item.url}/>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   );
-    }
+    
 
 }
 export default Hero;
--- Neededforwebsite/AllComponent/src/Components/Items/Item.jsx
+++ Neededforwebsite/AllComponent/src/Components/Items/Item.jsx
@@ -1,15 +1,55 @@
- import "./Item.css";
+ import { useEffect, useState } from "react";
+import "./Item.css";
  function Item(props){
+    const {name,url}=props;
+    const [pokemon, setPokemon] = useState(null);
+    const [loading,setLoading]=useState(true);
+    const[error,setError]=useState("");
+    const fetchApi = (url) => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                setPokemon(data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                // console.log(err);
+                setError("Error fetching data");
+                setLoading(false);
+            });
+    };
+    useEffect(() => {
+        fetchApi(url);
+    }, []);
+
+    
+    if(loading){
+        return(
+            <div className="item">
+                <figure>
+                    <img src="https://cdn.dribbble.com/users/1162073/screenshots/3848914/media/0f1a2b5c7d8e6f9b0c3a4d2e5f1a2b5c.gif" alt="" />
+                </figure>
+                <h1>Loading...</h1>
+            </div>
+        )
+    }
+    if(error){
+        return(
+            <div className="item">
+                <figure>
+                    <img src="https://gifdb.com/images/high/pokemon-caterpie-your-a-bug-z3pgnijce9rtxa7j.webp" alt="" />
+                </figure>   
+                <h1>{error}</h1>
+            </div>
+        )
+    }
 return(
     <div className="item">
-        <ul className="item-list">
-            <li>
                 <figure>
-                    <img src={props.apiData.sprites.other.dream_world.front_default} alt="" />
+                    <img src={pokemon.sprites.other.dream_world.front_default} alt="" />
                 </figure>
-                <h1>{props.apiData.name}</h1>
-            </li>
-        </ul>
+                <h1 className="uppercase">{pokemon.name}</h1>
+            
     </div>
 )
  }
